60461fe5edeef63faadc34756f4bbdf3e025c439
yago <me@yago.ninja>
2025-02-22 16:03:28 +0100

    feat(cmd): add config loading for load balancer binary

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/yago-123/galelb/pkg/lb_net/node_manager"
+
 	"github.com/sirupsen/logrus"
-	"github.com/yago-123/galelb/pkg/lb_net"
+	lbConfig "github.com/yago-123/galelb/config/lb"
 	"github.com/yago-123/galelb/pkg/ring"
 	"github.com/yago-123/galelb/pkg/routing"
 )
 
 var logger = logrus.New()
+var cfg *lbConfig.Config
 
 func main() {
 	logger.SetLevel(logrus.DebugLevel)
+
+	// Execute the root command
+	Execute(logrus.New())
+
+	cfg.Logger.Infof("starting load balancer with config: %v", cfg)
 
 	// Create consistent hashing with 5 virtual nodes per real node
 	ch, err := ring.New(ring.Crc32Hasher, 5)
@@ -22,14 +30,13 @@ func main() {
 	}
 
 	// Load dummy eBPF program
-	router := routing.New(logger, "eno1")
+	router := routing.New(cfg.Logger, "eno1", 8080)
 	if err := router.LoadRouter(); err != nil {
-		log.Fatalf("failed to load router: %s", err)
+		log.Fatalf("failed to load router, ensure you have the required permissions: %s", err)
 	}
 
-	// create gRPC server for managing nodes
-	server := lb_net.New(logger)
-
+	// Create gRPC server for managing nodes
+	server := node_manager.New(cfg, 50051)
 	server.Start()
 
 	// Add some nodes
