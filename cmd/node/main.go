0aa99430974ce3f398ddfea762df10c40c3a8417
yago <me@yago.ninja>
2025-03-08 14:10:52 +0200

    feat(cmd/node): send concurrent health checks to all load balancers

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net"
+	"sync"
 	"time"
 
 	"github.com/yago-123/galelb/pkg/util"
@@ -24,6 +25,7 @@ var cfg *nodeConfig.Config
 
 func main() {
 	var err error
+	var wg sync.WaitGroup
 
 	Execute(logrus.New())
 
@@ -39,18 +41,19 @@ func main() {
 		// If the IP is not set, resolve the hostname via multicast DNS or regular DNS
 		if address.IP == "" && address.Hostname != "" {
 			ips := []net.IP{}
+
+			ctx, cancel := context.WithTimeout(context.Background(), HostnameResolveTimeout)
+			defer cancel()
+
 			if util.IsMultiCastDNS(address.Hostname) {
-				ips, err = util.ResolveMulticastDNS(address.Hostname)
+				ips, err = util.ResolveMulticastDNS(ctx, address.Hostname)
 				if err != nil {
 					cfg.Logger.Warnf("failed to resolve multicast DNS: %v", err)
 				}
 			} else if !util.IsMultiCastDNS(address.Hostname) {
-				ctx, cancel := context.WithTimeout(context.Background(), HostnameResolveTimeout)
-				defer cancel()
-
 				ips, err = util.ResolveDNS(ctx, address.Hostname, "127.0.0.1:53")
 				if err != nil {
-					cfg.Logger.Fatalf("failed to resolve hostname from configuration %s: %v", address.Hostname, err)
+					cfg.Logger.Warnf("failed to resolve hostname from configuration %s: %v", address.Hostname, err)
 				}
 			}
 
@@ -58,6 +61,8 @@ func main() {
 				cfg.Logger.Fatalf("no IP addresses found for hostname: %s", address.Hostname)
 			}
 
+			cfg.Logger.Debugf("resolved hostname %s to IP %s", address.Hostname, ips[0].String())
+			// todo(): this will not overwrite the original configuration
 			address.IP = ips[0].String()
 		}
 
@@ -77,19 +82,33 @@ func main() {
 		normalizedTime := time.Duration(executionCfg.HealthCheckTimeout) * time.Nanosecond
 		// todo(): change this to a more accurate value
 		healthPeriod := normalizedTime / 2
-		for {
-			<-time.After(healthPeriod)
 
-			ctx, cancel := context.WithTimeout(context.Background(), normalizedTime)
-			defer cancel()
+		// Increase work group to wait for the health status goroutine and spawn health check reporter
+		wg.Add(1)
+		go func() {
+			// Ensure Done is called when the goroutine finishes
+			defer wg.Done()
 
-			if err = client.ReportHealthStatus(ctx, &v1Consensus.HealthStatus{
-				Service: "gale-node",
-				Status:  uint32(v1Consensus.Serving),
-				Message: "Serving requests goes brrrrr",
-			}); err != nil {
-				cfg.Logger.Errorf("failed to report health status: %v", err)
+			// Report health status to the load balancer
+			for {
+				ctx, cancel := context.WithTimeout(context.Background(), normalizedTime)
+				defer cancel()
+
+				if err = client.ReportHealthStatus(ctx, &v1Consensus.HealthStatus{
+					Service: "gale-node",
+					Status:  uint32(v1Consensus.Serving),
+					Message: "Serving requests goes brrrrr",
+				}); err != nil {
+					cfg.Logger.Errorf("failed to report health status: %v", err)
+				}
+
+				cfg.Logger.Debugf("reported health status to %s:%d", address.IP, address.Port)
+
+				<-time.After(healthPeriod)
 			}
-		}
+		}()
 	}
+
+	// Wait for all goroutines to finish
+	wg.Wait()
 }
