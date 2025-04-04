729a2a5299cd4d67e9e8bb76ef8df8cd73d1a18d
yago <me@yago.ninja>
2025-04-04 16:47:28 +0300

    feat(xdp/tc): adjust DNAT to ignore requests that come from port different than 8080 and replace saddr before forwarding to backend

--- pkg/routing/router.c
+++ pkg/routing/router.c
@@ -1,81 +1,101 @@
 // +build ignore
-
 #include <linux/bpf.h>
+#include <linux/pkt_cls.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <linux/in.h>
 #include <linux/tcp.h>
-#include <linux/if_packet.h>
-#include <linux/ptrace.h>
-#include <linux/bpf_common.h>
-#include <bpf/bpf_endian.h>
+
 #include <bpf/bpf_helpers.h>
 
-#include "constants.h"
 
-// defined in pkg/common/
-#include "common.h"
+#define BACKEND_IP 0xC0A80070  // 192.168.0.112 (fixed Backend)
+#define LB_IP 0xC0A800F2       // 192.168.0.242 (fixed Load Balancer)
+
+// represent protocol number for TCP
+#define IPPROTO_TCP 6
 
 struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, MAX_NUMBER_VIRTUAL_NODE_ENTRIES);
-    __type(key, __u32);
+    __uint(type, BPF_MAP_TYPE_LRU_HASH);
+    __uint(max_entries, 65536);
+    __type(key, struct iphdr);
     __type(value, __u32);
-} xdp_array_map SEC(".maps");
+} snat_map SEC(".maps");
 
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, MAX_NUMBER_VIRTUAL_NODE_ENTRIES);
-    __type(key, __u32);
-    __type(value, ip_port_key);
-} xdp_hash_map SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, __u32);  // Source IP
-    __type(value, __u32); // NAT-ed IP
-} dnat_map SEC(".maps");
-
-SEC("tc")
+SEC("tc_ingress")  // Attach this to eth0 (incoming traffic)
 int dnat_prog(struct __sk_buff *skb) {
-    // Ensure safe access to socket buffer data
     void *data = (void *)(long)skb->data;
     void *data_end = (void *)(long)skb->data_end;
 
-    // Verify validity of ethernet bounds
     struct ethhdr *eth = data;
     if ((void *)(eth + 1) > data_end) return TC_ACT_OK;
 
-    // Verify that it's IPV4 packet
-    // todo(): add support for IPv6
     if (eth->h_proto != __constant_htons(ETH_P_IP)) return TC_ACT_OK;
 
-    // Verify validity of IP header
     struct iphdr *ip = (void *)(eth + 1);
     if ((void *)(ip + 1) > data_end) return TC_ACT_OK;
 
-    // Verify that it's TCP packet
-    // todo(): add support for UDP
     if (ip->protocol != IPPROTO_TCP) return TC_ACT_OK;
 
-    // Verify validity of TCP header
     struct tcphdr *tcp = (void *)(ip + 1);
     if ((void *)(tcp + 1) > data_end) return TC_ACT_OK;
 
-    // Check if DNAT exists for the source IP
-    __u32 *new_daddr = bpf_map_lookup_elem(&dnat_map, &ip->daddr);
-    if (!new_daddr) return TC_ACT_OK;
+    // Convert from network byte order and check if the dest port is 8080
+    if (tcp->dest != __constant_htons(8080)) {
+        return TC_ACT_OK;  // Ignore if not port 8080
+    }
 
-    // Modify destination IP
+    // Keep track of the original destination and source address
     __u32 old_daddr = ip->daddr;
-    ip->daddr = *new_daddr;
+    __u32 old_saddr = ip->saddr;
 
-    // Update checksums to account for the new destination IP
-    bpf_l3_csum_replace(skb, offsetof(struct iphdr, check), old_daddr, *new_daddr, sizeof(__u32));
-    bpf_l4_csum_replace(skb, offsetof(struct tcphdr, check), old_daddr, *new_daddr, sizeof(__u32));
+    // Modify the destination address to the backend IP
+    ip->daddr = __constant_htonl(BACKEND_IP);
+    // Modify the source address to the load balancer to prevent breaking the connection
+    ip->saddr = __constant_htonl(LB_IP);
+
+    // Update the checksum for both IP and TCP headers
+    bpf_l3_csum_replace(skb, offsetof(struct iphdr, check), old_daddr, ip->daddr, sizeof(__u32));
+    bpf_l4_csum_replace(skb, offsetof(struct tcphdr, check), old_daddr, ip->daddr, sizeof(__u32));
+
+    bpf_l3_csum_replace(skb, offsetof(struct iphdr, check), old_saddr, ip->saddr, sizeof(__u32));
+    bpf_l4_csum_replace(skb, offsetof(struct tcphdr, check), old_saddr, ip->saddr, sizeof(__u32));
 
     return TC_ACT_OK;
 }
 
-SEC("license") char _license[] = "GPL";
+SEC("tc_egress")  // Attach this to eth1 (forwarding to backend)
+int snat_prog(struct __sk_buff *skb) {
+    void *data = (void *)(long)skb->data;
+    void *data_end = (void *)(long)skb->data_end;
+
+    struct ethhdr *eth = data;
+    if ((void *)(eth + 1) > data_end) return TC_ACT_OK;
+
+    if (eth->h_proto != __constant_htons(ETH_P_IP)) return TC_ACT_OK;
+
+    struct iphdr *ip = (void *)(eth + 1);
+    if ((void *)(ip + 1) > data_end) return TC_ACT_OK;
+
+    if (ip->protocol != IPPROTO_TCP) return TC_ACT_OK;
+
+    struct tcphdr *tcp = (void *)(ip + 1);
+    if ((void *)(tcp + 1) > data_end) return TC_ACT_OK;
+
+    struct iphdr key = *ip;
+    __u32 new_saddr = __constant_htonl(LB_IP); // SNAT to LB IP
+
+    __u32 *orig_saddr = bpf_map_lookup_elem(&snat_map, &key);
+    if (orig_saddr) {
+        ip->saddr = *orig_saddr;
+    } else {
+        ip->saddr = new_saddr;
+        bpf_map_update_elem(&snat_map, &key, &ip->saddr, BPF_ANY);
+    }
+
+    bpf_l3_csum_replace(skb, offsetof(struct iphdr, check), key.saddr, ip->saddr, sizeof(__u32));
+    bpf_l4_csum_replace(skb, offsetof(struct tcphdr, check), key.saddr, ip->saddr, sizeof(__u32));
+
+    return TC_ACT_OK;
+}
+
+char _license[] SEC("license") = "GPL";
